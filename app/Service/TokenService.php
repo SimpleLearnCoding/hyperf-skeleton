383ee3c788f95027e17800b4b807b6939f9ed5da
Linnzh <reg.lynnzh@gmail.com>
2023-03-08 14:40:36 +0800

    ✨ feat: Login

---
+++ app/Service/TokenService.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Redis\Redis;
+
+/**
+ * Class TokenService
+ *
+ * @author  linnzh
+ * @created 2023/3/8 13:52
+ */
+class TokenService
+{
+    public const USER_LOGIN_ACCESS_TOKEN_PREFIX = 'user:login_access_token:';
+
+    #[Inject]
+    private Redis $redis;
+
+    public function generateAccessToken(array $data, int $expire = 43200)
+    {
+        $userInfo = $this->getUserInfo($data['username'], $data['password']);
+
+        $key = uuid();
+
+        $this->redis->set(self::USER_LOGIN_ACCESS_TOKEN_PREFIX . $key, json_encode($userInfo), $expire);
+
+        return ['access_token' => $key, 'expires_in' => $expire];
+    }
+
+    private function getUserInfo(string $username, string $password)
+    {
+        // TODO 获取用户信息
+        return [
+            'username' => $username,
+        ];
+    }
+}
---
+++ extends/Functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function uuid(string $prefix = ''): string
+{
+    $chars = md5(uniqid((string) mt_rand(), true));
+    $uuid = substr($chars, 0, 8) . '-'
+        . substr($chars, 8, 4) . '-'
+        . substr($chars, 12, 4) . '-'
+        . substr($chars, 16, 4) . '-'
+        . substr($chars, 20, 12);
+
+    return $prefix . $uuid;
+}
